5f1fd480a82e4a2b688d95289b8ad94ad87dfa60
FelixFalkovsky <falkovskyfelix@gmail.com>
2021-10-25 19:35:06 +0300

    [Kotlin] Variables and constants

--- Kotlin/ComparisonKotlinSwift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
+++ Kotlin/ComparisonKotlinSwift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
@@ -5,13 +5,32 @@ import org.junit.Test
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ Сравнение языков Kotlin 1.5.31 и Swift 5.5
+ ссылка на GitHab Swift
+ https://github.com/FelixFalkovsky/Comparison-Swift-Kotlin
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+}
+
+class Something {
+    val some = ""
+    val user: String = ""
+    val name = null
+    val welcome = some?.length
+    val value = some?.length ?: 30
+    val forced = some!! // принудительное разворачивание
+    @Test
+    fun show_result() {
+        some?.let { some ->
+            println("Результат ${some.length}")
+        }
+
+        if (some != null) {
+            println(some)
+        }
     }
 }
