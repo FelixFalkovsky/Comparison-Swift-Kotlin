0a840080d939e66c5c825c9304bdf17c78cd2bf5
FelixFalkovsky <falkovskyfelix@gmail.com>
2021-10-30 12:02:05 +0300

    update plan

--- Kotlin/Comparison-Kotlin-Swift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
+++ Kotlin/Comparison-Kotlin-Swift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
@@ -46,55 +46,137 @@ class ExampleUnitTest {
 /**
  * Форматирование кода // Скобки (круглые и фигурные)
  */
+class CodeFormattingUnitTest {
+    val hello = "Hello World"
+    @Test
+    fun printFormatinCode(value: Int): Int = value + 1
 
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $hello")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Управляющие конструкции // Pattern Matching
  */
-
+class ControlConstructsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Типы
  */
-
+class TypeUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Строковая интерполяция
  */
-
+class StringInterpolationUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Интервалы (Ranges)
  */
-
+class IntervalsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Коллекции
  */
-
+class CollectionUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Итерирование коллекций
  */
-
+class IteratingCollectionsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Процедуры и функции
  */
-
+class ProceduresAndFunctionsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Интерфейсы
  */
-
+class InterfacesUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Конструкторы
  */
-
+class ConstructsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Инстанцирование объектов
  */
-
+class InstantiatingObjectsUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Расширения классов
  */
-
+class ExtensionUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Простые объекты и Singleton’ы
  */
-
+class ObjectUnitTest {
+    fun myPrintTest() {
+        println("_______________________TEST____________________________")
+        println("Результат $")
+        println("_______________________TEST____________________________")
+    }
+}
 /**
  * Перегрузка операторов
  */
