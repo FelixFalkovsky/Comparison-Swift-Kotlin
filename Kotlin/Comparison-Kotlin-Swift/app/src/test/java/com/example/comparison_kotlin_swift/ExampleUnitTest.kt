5f134d2e4b235a38347ff13101c003f2d22405f7
FelixFalkovsky <falkovskyfelix@gmail.com>
2021-11-05 20:03:31 +0300

    add fun map filter test

--- Kotlin/Comparison-Kotlin-Swift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
+++ Kotlin/Comparison-Kotlin-Swift/app/src/test/java/com/example/comparison_kotlin_swift/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.example.comparison_kotlin_swift
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.abs
+import kotlin.math.max
 
 /**
  * Сравнение языков Kotlin и Swift
@@ -49,6 +51,12 @@ class ExampleUnitTest {
 class CodeFormattingUnitTest {
     val hello = "Hello World"
     val number: Int = 0
+    val numberArray = arrayOf(1, 2, 3, 4, 5, -100, -1024)
+    val myArray =
+        mutableListOf("Apple", "Chery", "Banan") // mutableListOf - для Kotlin / listOf - Java
+    val fruitsArray: List<String> = myArray                // List - только для чтения
+    val input =
+        "Wissenschaft der automatischen Verarbeitung von Informationen mit Hilfe von Rechenanlagen."
 
     fun increase(value: Int): Int = value + 1 // увеличиваем значение
 
@@ -62,6 +70,64 @@ class CodeFormattingUnitTest {
         }
     }
 
+    fun sortedFrutis() {
+        while (myArray != null) {
+            myArray.add("Raspberries")
+            myArray.removeAt(3)
+            myArray.add(1, "Coconut")
+            println("Результат doWhile ${myArray.toString()}")
+            println("Результат doWhile ${fruitsArray.toString()}")
+            return
+        }
+    }
+
+    fun resultMap() {
+        val result = numberArray.map {
+            //  it * 10
+            it + 100
+        }
+        val resultV = numberArray.map {
+            // v -> v - 1
+                v ->
+            v + 100
+        }
+
+        var assorti = myArray.map { "The fruit " + it }
+
+        println(resultV.joinToString(","))
+        println(result.joinToString(","))
+        println(assorti.joinToString(","))
+    }
+
+    fun resultFlatMap() {
+        val resultFlatMap = numberArray.map {
+            0..it
+            // используем map + flatten / альтернатива flatMap()
+
+        }.flatten()
+
+        val reloadFlatMap = numberArray.flatMap {
+            0..it
+        }
+
+        println("Результат  ${resultFlatMap.joinToString(",")}")
+    }
+
+    fun resultFold() {
+        val result = numberArray.fold(Int.MIN_VALUE) { acc, i ->
+            println("Результат acc: $acc, i: $i")
+            max(acc, i)
+        }
+        println("Результат minValue ${result}")
+    }
+
+    fun resultFilter() {
+        val result = numberArray.filter {
+            abs(it) < 100
+        }
+        println("Результат resultFilter ${result}")
+    }
+
     fun doWhile() {
         var number = 0
         do {
@@ -72,9 +138,48 @@ class CodeFormattingUnitTest {
         println("Результат doWhile $number")
     }
 
+    fun filterWord(c: Char): (String) -> String = { s ->
+        s.takeIf {
+            it.endsWith(c)
+        }?.dropLast(1) ?: s
+    }
+
+    val words = input.split( " ")
+        .map(String::lowercase)
+        .map(filterWord(','))
+        .map(filterWord('.'))
+
+    val wordsCount = words.groupingBy {
+        it
+    }.eachCount()
+
+    val shortestLenght = words.minByOrNull { it.length }?. length
+
+    val shortestWords = words.filter {
+        it.length == shortestLenght
+    }
+
+    fun result() {
+        wordsCount.map {
+                (k, v) -> "$k: $v"
+        }.forEach(:: println)
+        println(shortestWords.joinToString(" , "))
+    }
+
+
+
+
+
     @Test
     fun myPrintTest() {
         println("_______________________TEST____________________________")
+        result()
+        println("_______________________TEST____________________________")
+        resultMap()
+        resultFold()
+        resultFlatMap()
+        resultFilter()
+        println("Результат sortedFrutis ${sortedFrutis()}")
         println("Результат doWhile ${doWhile()}")
         println("Результат statements ${statements()}")
         println("Результат $hello")
@@ -106,7 +211,7 @@ class ControlConstructsUnitTest {
             number.isEven() -> println("Результат $number")
         }
     }
-    fun Int.isOdd() = this % 2 != 0
+    fun Int.isOdd() = this % 2 != 0 //this = self % = /
     fun Int.isEven() = this % 2 == 0
     @Test
     fun myPrintTest() {
